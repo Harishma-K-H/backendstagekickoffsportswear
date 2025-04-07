0b5bf4f5fa05df10fdb76bcc6a520d87c4c2fc60
Kickoff Dev <dev@kickoffsportswear.app>
2025-04-07 09:32:53 +0000

    Updated Order/urls.py from origin/server

--- Order/urls.py
+++ Order/urls.py
@@ -3,15 +3,15 @@ from .views import DetailedOrderAPIView,CreateOrderAPIView,GetNextOrderNumberAPI
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('api_order/',CreateOrderAPIView.as_view(),name='orders_list'),
-    path('update-order-item/', OrderItemUpdateView.as_view(), name='update-order-item'),
-    path('order/details/<int:order_id>/',DetailedOrderAPIView.as_view(),name='order_list'),
-    path("order_no_generate/", GetNextOrderNumberAPIView.as_view(), name="order_numbers"),
+    path('api/api_order/',CreateOrderAPIView.as_view(),name='orders_list'),
+    path('api/update-order-item/', OrderItemUpdateView.as_view(), name='update-order-item'),
+    path('api/order/details/<int:order_id>/',DetailedOrderAPIView.as_view(),name='order_list'),
+    path("api/order_no_generate/", GetNextOrderNumberAPIView.as_view(), name="order_numbers"),
     path('api/payment/',OrderPaymentAPI.as_view(),name='order_payment'),
-    path('invoice_list/',InvoiceList.as_view(),name='invoice_list'),
-    path('invoices/<str:invoice_id>/', InvoiceView.as_view(), name='invoice-detail'),
+    path('api/invoice_list/',InvoiceList.as_view(),name='invoice_list'),
+    path('api/invoices/<str:invoice_id>/', InvoiceView.as_view(), name='invoice-detail'),
     # path("invoice_no_generate/", GetInvoiceNumberAPIView.as_view(), name="invoice_number"),
-    path('payment_details/<int:order_id>/',OrderPaymentDetails.as_view(),name='order_payment_details')
+    path('api/payment_details/<int:order_id>/',OrderPaymentDetails.as_view(),name='order_payment_details')
   
 
 
